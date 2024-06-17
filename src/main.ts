779a7c9555c09c7ae26540b1ecf20ba24ca116a6
Anatoli Gavrash <gaver5@yandex.ru>
2024-06-17 22:25:55 +0300

    Add form and logic for modal window

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 import './index.scss';
+import {createForm} from './utils/dom.ts';
+
+(() => {
+  const modal = document.querySelector('.modal');
+  const modalButton = document.querySelector('.content__left-button');
+
+  if (modal) {
+    modal.append(createForm());
+    modal.addEventListener('click', (e) => {
+      const element = e.target as HTMLButtonElement;
+      
+      if (element.classList.contains('modal')) {
+        element.classList.remove('show');
+      }
+    });
+  }
+
+  modalButton?.addEventListener('click', () => {
+    modal?.classList.add('show');
+  });
+})();
